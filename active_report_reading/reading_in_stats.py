e19cc897a719bd2ea56cda9929dfa2d449d2ebae
Kyle Koeller <kjkoeller@bsu.edu>
2022-07-17 19:11:41 -0500

    Any number of input files
    Allowed the code to allow the user to enter in any number of days for an active reporting counting. It then adds all the unique keys and values together into 1 singular output text file with those keys and totaled values.

--- active_report_reading/reading_in_stats.py
+++ active_report_reading/reading_in_stats.py
@@ -7,8 +7,10 @@ Searches through an active report of all sales for a given day and counts the un
 that key appears with a value. Since the report reports all values twice, I divide the total number of the value by 2
 """
 
+# import packages required
 import pandas as pd
 from collections import Counter
+from os import path
 
 
 def main():
@@ -18,22 +20,40 @@ def main():
 
     :return: nothing
     """
-    # import file, might want this to be a user_input
-    df = pd.read_csv("active_report_June_14.txt", delimiter="\t", skiprows=5, header=None)
-    # list for the item names
-    item_names = list(df[2])
 
-    # creates a dictionary for the item names (key) and the total of that item in the list (value)
-    count = Counter(item_names)
+    # allows for any number of days to be added together
+    n = int(input("How many days are there: "))
+    count = {}
+    for i in range(n):
+        while True:
+            # makes sure the file pathway is real and points to some file
+            # (does not check if that file is the correct one though)
+            try:
+                # an example pathway for the files
+                # C:\Users\Kyle\OneDrive\Computer-Science\Personal_Projects\BFB_Stats\report.txt
+                file = input("Enter file path %d: " % (i + 1))
+                if path.exists(file):
+                    break
+                else:
+                    continue
+            except FileNotFoundError:
+                print("Please enter a correct file path")
+        # import file, might want this to be a user_input
+        df = pd.read_csv(file, delimiter="\t", skiprows=5, header=None)
+        # list for the item names
+        item_names = list(df[2])
+
+        # creates a dictionary for the item names (key) and the total of that item in the list (value)
+        count = Counter(count) + Counter(item_names)
 
     # opens a file with this name to begin writing to the file but makes sure it has a .txt ending for easy reading post
     output_test = None
     while not output_test:
-        output_file = input("What is the output file name for the regression tables (either .txt or .tex): ")
+        output_file = input("What is the output file name for the stats to go to (ends with .txt): ")
         if output_file.endswith(".txt"):
             output_test = True
         else:
-            print("This is not an allowed file output. Please make sure the file has the extension .txt or .tex.")
+            print("This is not an allowed file output. Please make sure the file has the extension .txt")
             print()
 
     # noinspection PyUnboundLocalVariable
