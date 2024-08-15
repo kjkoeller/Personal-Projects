97176627e27cec39d3186e06287c5ed12aa56268
Kyle Koeller <kjkoeller@bsu.edu>
2024-08-15 09:56:05 -0500

    Update dqn_agent.py

---
+++ traffic_sim/dqn_agent.py
@@ -0,0 +1,66 @@
+# dqn_agent.py
+import numpy as np
+import random
+from collections import deque
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class DQN(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, action_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
+
+class Agent:
+    def __init__(self, state_dim, action_dim):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
+        self.learning_rate = 0.001
+        self.model = DQN(state_dim, action_dim)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.criterion = nn.MSELoss()
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_dim)
+        state = torch.tensor(state, dtype=torch.float32)
+        act_values = self.model(state)
+        return torch.argmax(act_values).item()
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                next_state = torch.tensor(next_state, dtype=torch.float32)
+                target = (reward + self.gamma * torch.max(self.model(next_state)).item())
+            state = torch.tensor(state, dtype=torch.float32)
+            target_f = self.model(state)
+            target_f[action] = target
+            self.optimizer.zero_grad()
+            loss = self.criterion(target_f, state)
+            loss.backward()
+            self.optimizer.step()
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_state_dict(torch.load(name))
+
+    def save(self, name):
+        torch.save(self.model.state_dict(), name)
